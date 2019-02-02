e6bc88ccdacec03be760781c8720e0d56c40aae1
George Singer <george.w.singer@gmail.com>
2019-02-02 14:54:44 -0500

    Incremental commit: functions implemented up to processCursorMotion.

--- src/TinyWL.hs
+++ src/TinyWL.hs
@@ -6,6 +6,7 @@ module TinyWL where
 import           Control.Concurrent.STM.TVar
 import           Control.Monad
 import           Control.Monad.STM
+import           Data.Maybe
 
 import           Foreign
 import           Foreign.C.Types
@@ -40,12 +41,17 @@ import           Graphics.Wayland.WlRoots.Input.Pointer
 import           Graphics.Wayland.WlRoots.Cursor
 
 import           System.Clock
+import           Control.Monad.Extra
 
 -- Turn on inline-C for select calls that hsroots doesn't provide.
 C.initializeTinyWLCtxAndIncludes
 
-data OutputLayoutCoordinates = OutputLayoutCoordinates (Int, Int)
-data SurfaceLocalCoordinates = SurfaceLocalCoordinates (Double, Double) -- I believe each Double ranges from 0 to 1
+-- | TinyWL's C implementation uses Int for surface height/width and view location in output layout space; the rest of the time it uses Double.
+-- | In Haskell we'll just always use Double but make things more type safe.
+-- | TODO: Ensure this doesn't lead to unintended consequences, such as making a surface have non-integer volume or moving a surface into the middle of a pixel.
+data OutputLayoutCoordinates = OutputLayoutCoordinates (Double, Double) -- Depending upon the context, we can be working with Int or Double.
+data SurfaceLocalCoordinates = SurfaceLocalCoordinates (Double, Double) -- SurfaceLocalCoordinates are at the same scale as OutputLayoutCoordinates, despite being in Double
+data SubSurfaceLocalCoordinates = SubSurfaceLocalCoordinates (Double, Double) -- "
   
 data TinyWLCursorMode = TinyWLCursorPassthrough | TinyWLCursorMove |  TinyWLCursorResize
 
@@ -66,8 +72,7 @@ data TinyWLServer = TinyWLServer { _tsBackend :: Ptr Backend
                                  , _tsKeyboards :: TVar [TinyWLKeyboard]
                                  , _tsCursorMode :: TinyWLCursorMode
                                  , _tsGrabbedView :: TinyWLView
-                                 , _tsGrabX :: Double
-                                 , _tsGrabY :: Double
+                                 , _tsGrab :: TVar SurfaceLocalCoordinates -- The surface-local point at which a surface is grabbed.
                                  , _tsGrabWidth :: Int
                                  , _tsResizeEdges :: Int
                                  , _tsOutputLayout :: Ptr WlrOutputLayout
@@ -88,7 +93,7 @@ data TinyWLView = TinyWLView { _tvServer :: TinyWLServer
                              , _tvRequestMove :: WlListener ()
                              , _tvRequestResize :: WlListener ()
                              , _tvMapped :: Bool
-                             , _tvOutputLayoutCoordinates :: OutputLayoutCoordinates -- Denotes top-left corner x-coordinate of view in output layout space
+                             , _tvOutputLayoutCoordinates :: TVar OutputLayoutCoordinates -- Denotes top-left corner of view in OutputLayoutCoordinates
                              }
 
 data TinyWLKeyboard = TinyWLKeyboard { _tkServer :: TinyWLServer
@@ -102,6 +107,12 @@ data RenderData = RenderData { _rdOutput :: Ptr WlrOutput
                              , _rdView :: TinyWLView
                              , _rdWhen :: TimeSpec
                              }
+
+-- | Helper function to convert Haskell timestamps to milliseconds (which wlroots expects) for C marshalling.
+toMsec32 :: TimeSpec -> IO (Word32)
+toMsec32 timeSpec = do
+  let msec = fromIntegral $ toNanoSecs timeSpec `div` 1000000
+  return msec
 
 -- | We will say that two views are "the same" when they have the same Ptr WlrXdgSurface (this is
 -- | is a bad idea long-term but for now will suffice; for example, we might have two
@@ -305,23 +316,113 @@ seatRequestCursor tinyWLServer = WlListener $ \ptrSetCursorEvent ->
 
      when (clientRequester == focusedClient) $ setCursorSurface cursor surface hotspotX hotspotY -- Since this event can be called by any client, we first make sure the client that triggered it actually has seat focus
 
-viewAt :: TinyWLView -> OutputLayoutCoordinates -> IO (Maybe (Ptr WlrSurface, SurfaceLocalCoordinates))
--- viewAt :: TinyWLView -> Double -> Double -> Ptr (Ptr WlrSurface) -> Double -> Double -> Bool
-viewAt = undefined
--- [[file:~/hsroots/src/Graphics/Wayland/WlRoots/XdgShell.hsc::xdgSurfaceAt%20::%20Ptr%20WlrXdgSurface%20->%20Double%20->%20Double%20->%20IO%20(Maybe%20(Ptr%20WlrSurface,%20Double,%20Double))]]
+-- | This function takes a pair of output layout coordinates (for a single view) and
+-- | inspects if there are any XDG toplevel "subsurfaces" at those coordinates (this
+-- | could either be a true "subsurface" -- like a popup -- or the trivial subsurface
+-- | [the parent surface associated with the view]). We return the wlr_surface (if there is one)
+-- | associated with the "subsurface", and convert the output layout coordinates to the
+-- | corresponding subsurface coordinates relative to the subsurface.
+viewAt :: TinyWLView -> OutputLayoutCoordinates -> IO (Maybe (Ptr WlrSurface, SubSurfaceLocalCoordinates))
+viewAt tinyWLView (OutputLayoutCoordinates (lx, ly)) = do                                                                     -- (lx, ly) denote the cursor's position in output layout space
+  OutputLayoutCoordinates (topleftX, topleftY) <- atomically $ readTVar (_tvOutputLayoutCoordinates tinyWLView)                                  -- (topleftX, topleftY) denotes the view's top-left corner in output layout space
+  let SurfaceLocalCoordinates (sx, sy) = SurfaceLocalCoordinates (lx - topleftX, ly - topleftY) -- (sx, sy) denote the cursor's position relative to the tinyWLView's surface space
 
-desktopViewAt :: TinyWLServer -> Double -> Double -> Ptr (Ptr WlrSurface) -> Double -> Double -> TinyWLView
-desktopViewAt  = undefined
+  -- In tinywl.c, there is a dangling `struct wlr_surface_state *state = &view->xdg_surface->surface->current;` whose purpose is unclear.
 
+  let xdgSurface = (_tvXdgSurface tinyWLView)
+  maybeSubSurface <- xdgSurfaceAt xdgSurface sx sy
+  case maybeSubSurface of
+       Nothing                     -> return $ Nothing
+       Just (subSurface, ssx, ssy) -> return $ Just (subSurface, SubSurfaceLocalCoordinates (ssx, ssy))                       -- (ssx, ssy) denote the cursor's position relative to the subsurface's space.
+
+-- | This function is similar to `viewAt`, except it inspects *all* of the server's
+-- | views for subsurfaces and returns a view. This relies on the server's list of
+-- | views being ordered from top-to-bottom (and this in turn explains why in, i.e.,
+-- | `focusView` we ensure to mutate the active view to the head of our server's list
+-- | of views).
+desktopViewAt :: TinyWLServer -> OutputLayoutCoordinates -> IO (Maybe TinyWLView)
+desktopViewAt  tinyWLServer outputLayoutCoordinates = do
+  listOfViews             <- atomically $ readTVar (_tsViews tinyWLServer)
+  maybeViewWithSubsurface <- findM hasSubsurface listOfViews
+  return maybeViewWithSubsurface
+  where hasSubsurface :: TinyWLView -> IO (Bool)
+        hasSubsurface tinyWLView = do
+          maybeSurfaceData <- viewAt tinyWLView outputLayoutCoordinates 
+          case maybeSurfaceData of
+               Just _  -> return True
+               Nothing -> return False
+
+-- | This function mutates the server's grabbed view location (specifically: the
+-- | location of its top-left corner in output layout space) based on (i) the
+-- | point at which the view was grabbed (in surface local coordintaes) and (ii)
+-- | the point at which the cursor is currently located (in output layout space).
+-- | Keeping track of all of these coordinate systems can be confusing so to make
+-- | things as easy as possible we add type annotations even when not strictly
+-- | required.
+-- | NOTE: The second argument isn't used in the C implementation.
 processCursorMove :: TinyWLServer -> TimeSpec -> IO ()
-processCursorMove = undefined
+processCursorMove tinyWLServer _ = do
+   grabbedViewTopLeft@(OutputLayoutCoordinates (grabbedViewLX, grabbedViewLY)) <- atomically $ readTVar (_tvOutputLayoutCoordinates (_tsGrabbedView tinyWLServer))
+   surfaceGrabPoint@(SurfaceLocalCoordinates (surfaceGrabPointSX, surfaceGrabPointSY)) <- atomically $ readTVar (_tsGrab tinyWLServer)
 
+   let cursor = (_tsCursor tinyWLServer)
+   cursorLX <- getCursorX cursor
+   cursorLY <- getCursorY cursor
+   let cursorPosition = OutputLayoutCoordinates (cursorLX, cursorLY)
+
+   let newGrabbedViewTopLeft = OutputLayoutCoordinates (cursorLX - surfaceGrabPointSX, cursorLY - surfaceGrabPointSY)
+   atomically $ writeTVar (_tvOutputLayoutCoordinates (_tsGrabbedView tinyWLServer)) newGrabbedViewTopLeft
+
+-- | Since VR requires specialized treatment of surface resizing, I'm waiting to
+-- | implement this until later.
 processCursorResize :: TinyWLServer -> TimeSpec -> IO ()
-processCursorResize = undefined
+processCursorResize _ _ = do
+  putStrLn "Window resizing is not yet implemented in haskell-tinywl!"
 
+-- | This function inspects the server's cursor mode, and either hands over to
+-- | processCursorMove or processCursorResize, or handles movement via (i) finding
+-- | the view under the cursor and passing responsibility to client or, if there is
+-- | no view, (ii) drawing the cursor image at the compositor level. There are some
+-- | subtleties in this function that may impact VR inputs, namely: (i) when a cursor
+-- | notifies a surface that it has entered it, it is automatically given focus and
+-- | (ii) when the cursor is not over any view, we repeatedly emit
+-- | "pointerClearFocus" events to ensure that button events aren't sent to surfaces
+-- | that previously had focus.
 processCursorMotion :: TinyWLServer -> TimeSpec -> IO ()
-processCursorMotion = undefined
--- let seat = (_tsSeat server)
+processCursorMotion tinyWLServer timeSpec = do
+  let cursorMode = (_tsCursorMode tinyWLServer)
+  case cursorMode of
+       TinyWLCursorMove -> processCursorMove tinyWLServer timeSpec
+       TinyWLCursorResize -> processCursorResize tinyWLServer timeSpec
+       TinyWLCursorPassthrough -> serverPassthroughMotion
+  where serverPassthroughMotion :: IO ()
+        serverPassthroughMotion = do
+          let seat = (_tsSeat tinyWLServer)
+          let cursor = (_tsCursor tinyWLServer)
+          let cursorManager = (_tsCursorManager tinyWLServer)
+          cursorLX <- getCursorX cursor
+          cursorLY <- getCursorY cursor
+          let cursorPosition = OutputLayoutCoordinates (cursorLX, cursorLY)
+          maybeViewAtPoint <- desktopViewAt tinyWLServer cursorPosition
+          case maybeViewAtPoint of
+                Nothing -> do
+                  xCursorSetImage cursorManager "left_ptr" cursor -- If there's no view, then the server is responsible for drawing the cursor image
+                  pointerClearFocus seat -- Clear the pointer focus so future buton events aren't set to the last client that had focus
+                Just viewAtPoint -> do
+                 (subSurfaceAtPoint, SubSurfaceLocalCoordinates (ssx, ssy))  <- fromJust <$> viewAt viewAtPoint cursorPosition
+                 surfaceMotion subSurfaceAtPoint ssx ssy -- The term "subsurface" here is somewhat misleading; this could be either be something like a popup or an ordinary surface associated with a view
+        surfaceMotion :: Ptr WlrSurface -> Double -> Double -> IO ()
+        surfaceMotion subSurfaceAtPoint ssx ssy = do
+            let seat = (_tsSeat tinyWLServer)
+            let seat' = toInlineC seat
+            lastFocusedSurface' <- [C.exp| struct wlr_surface * { $(struct wlr_seat * seat')->pointer_state.focused_surface } |] -- hsroots doesn't provide access to this data structure AFAIK
+            let lastFocusedSurface = toC2HS lastFocusedSurface'
+            let focusChanged = (lastFocusedSurface /= subSurfaceAtPoint)
+            msec32 <- toMsec32 timeSpec
+
+            -- Either tell the client we just entered the surface, or that we moved within the surface.
+            when focusChanged       $ pointerNotifyEnter seat subSurfaceAtPoint ssx ssy -- This automatically gives the surface "pointer focus", which is distinct from keyboard focus. TODO: Ensure this doesn't cause issue with VR, where we have two controllers potentially fighting for focus.
+            when (not focusChanged) $ pointerNotifyMotion seat msec32 ssx ssy
 
 serverCursorMotion :: WlListener ()
 serverCursorMotion = undefined
